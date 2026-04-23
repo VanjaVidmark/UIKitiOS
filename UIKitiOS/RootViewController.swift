4c86e7ff49f7e4b03f9e31ca6890edb9faf90eaa
Isabella Viksten <isabella.viksten@netlight.com>
2026-04-23 10:57:53 +0200

    Add Welcome label

--- UIKitiOS/RootViewController.swift
+++ UIKitiOS/RootViewController.swift
@@ -8,13 +8,29 @@
 import UIKit
 
 // MARK: RootViewController
-final class RootViewController: UIViewController {}
+final class RootViewController: UIViewController {
+    private lazy var label: UILabel = {
+       let label = UILabel()
+        label.text = String(localized: .signupWelcomeLabel)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+}
 
 // MARK: Override
 extension RootViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		view.backgroundColor = .red
+        
+        view.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            label.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            label.topAnchor.constraint(equalTo: view.topAnchor),
+            label.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+        
 	}
 }
 
